67e4f95bba1982bf75a6f0c1a1effc1e77ba57e0
Fahras <fahras@loyalto.id>
2025-03-16 09:49:25 +0700

    Fix: update ts movie

--- src/types/movieTypes.ts
+++ src/types/movieTypes.ts
@@ -1,18 +1,32 @@
 export interface Movie {
   adult: boolean;
   backdrop_path: string;
-  genre_ids: number[];
+  belongs_to_collection: null;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
   id: number;
+  imdb_id: string;
+  origin_country: string[];
   original_language: string;
   original_title: string;
   overview: string;
   popularity: number;
   poster_path: string;
   release_date: string;
+  revenue: number;
+  runtime: number;
+  status: string;
+  tagline: string;
   title: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
 }
 
 export interface MovieParams {
